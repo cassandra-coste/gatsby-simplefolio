ba6acd2d4006dc1e5dab5169fe8bf387098288a8
cassandra-coste <cgc266@nyu.edu>
2021-06-16 22:01:51 -0400

    Profile pic update

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'HeadshotSq.jpg',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
